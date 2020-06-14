8cccd4e5971a5a3c6637afbf2ff23b7906921b1b
Mahir Oberai <49322365+MahirOberai@users.noreply.github.com>
2020-06-14 16:48:07 +0800

    added ACC files
    added ACC files

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Canvas from '../../components/Canvas';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  Tabs,
+  Tab,
+} from 'carbon-components-react';
+
+const props = {
+  tabs: {
+    selected: 0,
+    triggerHref: '#',
+    role: 'navigation',
+  },
+  tab: {
+    href: '#',
+    role: 'presentation',
+    tabIndex: 0,
+  },
+};
+
+const LandingPage = () => {
+  return (
+    <div className="bx--grid bx--grid--full-width landing-page">
+      <div className="bx--row landing-page__banner">
+        <div className="bx--col-lg-16">
+          <Breadcrumb noTrailingSlash>
+            <BreadcrumbItem>
+              <a href="/">Click to reload (if need to clear canvas)</a>
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <h1 className="landing-page__heading">Area Control Centre</h1>
+          <Canvas />
+        </div>
+      </div>
+      <div className="bx--row landing-page__r2">
+        <div className="bx--col bx--no-gutter">
+          <Tabs {...props.tabs}>
+            <Tab {...props.tab} label="Equipment">
+              <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
+                <div className="bx--row landing-page__tab-content">
+                  <div className="bx--col-lg-16">List of equipment: hose x1</div>
+                </div>
+              </div>
+            </Tab>
+            <Tab {...props.tab} label="Number of Personel">
+              <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
+                <div className="bx--row landing-page__tab-content">
+                  <div className="bx--col-lg-16">3 figher fighters in squad 1</div>
+                </div>
+              </div>
+            </Tab>
+            <Tab {...props.tab} label="Don't click me">
+              <div className="bx--grid bx--grid--no-gutter bx--grid--full-width">
+                <div className="bx--row landing-page__tab-content">
+                  <div className="bx--col-lg-16">
+                    <Breadcrumb noTrailingSlash>
+                      <BreadcrumbItem>
+                        <a href="https://www.youtube.com/watch?v=xSZBAYXipUg">
+                          Don't click pls
+                        </a>
+                      </BreadcrumbItem>
+                    </Breadcrumb>
+                  </div>
+                </div>
+              </div>
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default LandingPage;
